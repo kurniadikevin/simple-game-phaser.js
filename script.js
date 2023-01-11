e91c018b0f0a9a70876a16e0a71d3d5a1540aef9
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-01-11 15:59:51 +0700

    add note

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ class GameScene extends Phaser.Scene{
         this.load.image('ground', 'assets/platform.jpg');
         this.load.image('bgImg2','./assets/tree-background01.png');
         this.load.image('stoneStep','./assets/stoneMedium.png');
+        
     }
 
 
@@ -50,6 +51,7 @@ class GameScene extends Phaser.Scene{
         gameState.step = this.physics.add.staticGroup();
     
         this.stepSetup();
+
        
         //ANIMATION FOR SPRITESHEET
         //run
@@ -137,7 +139,10 @@ class GameScene extends Phaser.Scene{
             
         }
        
-        //console.log(gameState.player.body.position.y);
+       //text caption
+       const caption= document.querySelector('.caption');
+       caption.textContent=this.levelKey;
+
     }
 }
 
@@ -163,7 +168,7 @@ class Level1 extends GameScene {
   class Level2 extends GameScene {
     constructor() {
       super('Level2')
-      this.levelStepSetup = [ [5,0],[6,0],[7,1],[48,1],[47,2],[46,3],[45,4],[44,5]];
+      this.levelStepSetup = [   [5,0],[6,0],[7,1],[9,3],[10,4],[10.5,4],[11,4],[13,4], [15,5], [5,0],[6,0],[7,1],[48,1],[47,2],[46,3],[45,4],[44,5]];
     }
   }
 

5855d2dbe0141c153a2fbcbce1c9b5c697d6a96e
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-01-12 17:18:38 +0700

    add trampoline

--- script.js
+++ script.js
@@ -23,10 +23,13 @@ class GameScene extends Phaser.Scene{
         this.load.image('bgImg1', 'assets/mountain-background.jpg');
         this.load.spritesheet('player', 'assets/flatboySS-noPadding.png'
         ,{ frameWidth: 350, frameHeight: 500 });
+       this.load.spritesheet('playerCrawl', 'assets/crawl-ss.png'
+        ,{ frameWidth: 481, frameHeight: 282 });
+
         this.load.image('ground', 'assets/platform.jpg');
         this.load.image('bgImg2','./assets/tree-background01.png');
         this.load.image('stoneStep','./assets/stoneMedium.png');
-        
+        this.load.image('trampoline','./assets/trampoline-crop.png');
     }
 
 
@@ -39,18 +42,26 @@ class GameScene extends Phaser.Scene{
 
         gameState.backgroundTree = this.add.image(960, 450, 'bgImg2').setScale(1);
         gameState.backgroundTreeMirror = this.add.image(2880, 450, 'bgImg2').setScale(1);
-      //  gameState.backgroundTreeMirror.flipX= true;
 
-        gameState.player = this.physics.add.sprite(50,400, 'player')
+        gameState.player = this.physics.add.sprite(50,600, 'player')
         .setScale(0.15);
+        
         
         gameState.platforms = this.physics.add.staticGroup();
         gameState.platforms.create(750, 819, 'ground').setScale(0.3).refreshBody();
         gameState.platforms.create(2251, 819, 'ground').setScale(0.3).refreshBody();
 
         gameState.step = this.physics.add.staticGroup();
+        gameState.trampoline = this.physics.add.staticGroup();
     
-        this.stepSetup();
+        // setup static object if there is one
+        if(this.levelStepSetup){
+          this.stepSetup();
+        }
+        
+        if(this.levelTrampolineSetup){
+          this.trampolineSetup();
+        }
 
        
         //ANIMATION FOR SPRITESHEET
@@ -77,12 +88,23 @@ class GameScene extends Phaser.Scene{
             frameRate: 10,
             repeat: -1
           });
+          
+          //crawl
+          this.anims.create({
+            key : 'crawl',
+            frames: this.anims.generateFrameNumbers('playerCrawl', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+          })
 
         gameState.player.setCollideWorldBounds(true);
         this.physics.add.collider(gameState.player, gameState.platforms);
         this.physics.add.collider(gameState.player, gameState.step);
-
-		gameState.cursors = this.input.keyboard.createCursorKeys();  
+        this.physics.add.collider(gameState.player,gameState.trampoline,function(){
+          gameState.player.setVelocityY(-gameState.jumpPower*1.5);
+        })
+        
+		    gameState.cursors = this.input.keyboard.createCursorKeys();  
         //camera setting
         this.cameras.main.setBounds(0, 0, gameState.width, gameState.height);
         this.physics.world.setBounds(0, 0, gameState.width, gameState.height);
@@ -90,18 +112,34 @@ class GameScene extends Phaser.Scene{
     }
 
 
-    // create step on game from input
+    // create static object in game
  createStep(xIndex, yIndex) {
           if (typeof yIndex === 'number' && typeof xIndex === 'number') {
             gameState.step.create((60 * xIndex),  720-(yIndex * 60), 'stoneStep').setScale(0.24).refreshBody();
           }
       }
+
+ createTrampoline(xIndex, yIndex) {
+   /*  if ( yIndex === 0){
+      gameState.trampoline.create((60 * xIndex),  720-((yIndex-1) * 60)-45, 'trampoline').setScale(0.0625).refreshBody();
+    } */
+    if (typeof yIndex === 'number' && typeof xIndex === 'number') {
+      gameState.trampoline.create((60 * xIndex),  720-(yIndex * 60)-45, 'trampoline').setScale(0.0625).refreshBody();
+    }
+    
+}
  
-      
+  //setup static object    
   stepSetup(){
     for( let i= 0 ; i< (this.levelStepSetup).length; i++){
         this.createStep(this.levelStepSetup[i][0],this.levelStepSetup[i][1]);
     }
+  }
+
+  trampolineSetup(){
+    for( let i= 0 ; i< (this.levelTrampolineSetup).length; i++){
+      this.createTrampoline(this.levelTrampolineSetup[i][0],this.levelTrampolineSetup[i][1]);
+  }
   }
 
 
@@ -118,7 +156,13 @@ class GameScene extends Phaser.Scene{
             gameState.player.flipX= false;
             gameState.player.anims.play('run',true);
 
-         } else if( (Phaser.Input.Keyboard.JustDown(gameState.cursors.space) || Phaser.Input.Keyboard.JustDown(gameState.cursors.up)) &&
+         } else if(gameState.cursors.down.isDown){
+            gameState.player.anims.play('crawl',true);// still error not collide and padding not reduced
+            
+            
+         }
+         
+         else if( (Phaser.Input.Keyboard.JustDown(gameState.cursors.space) || Phaser.Input.Keyboard.JustDown(gameState.cursors.up)) &&
         gameState.player.body.touching.down){
             gameState.player.setVelocityY(-gameState.jumpPower);
            
@@ -133,7 +177,6 @@ class GameScene extends Phaser.Scene{
         }
 
          if (gameState.player.body.position.y === 0){
-            alert(`you reach sky on ${this.levelKey}`);
             this.scene.stop(this.levelKey);
             this.scene.start(this.nextLevel[this.levelKey]);
             
@@ -162,21 +205,26 @@ class Level1 extends GameScene {
         [5,0],[6,0],[7,1],[9,3],[10,4],[10.5,4],[11,4],[13,4], [15,5],
         [17,7],[19,4],[20,4],[22,2],[5,3],[18,9]
                     ];
+      this.levelTrampolineSetup = [[9,3],[10,4],[10.5,4]];
     }
   }
 
   class Level2 extends GameScene {
     constructor() {
       super('Level2')
-      this.levelStepSetup = [   [5,0],[6,0],[7,1],[9,3],[10,4],[10.5,4],[11,4],[13,4], [15,5], [5,0],[6,0],[7,1],[48,1],[47,2],[46,3],[45,4],[44,5]];
+      this.levelStepSetup = [  [5,0],[6,1],[7,2],[8,3],[9,4]];
+   this.levelTrampolineSetup =[  [4,-1],  [5,0],[6,1],[7,2],[8,3],[9,4] ];
+    
+   
     }
   }
 
 
-var config = {
+let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    fps: {target: 60},
     physics: {
         default: 'arcade',
         arcade: {
